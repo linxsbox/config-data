a7298c6c09d6338e36ae441fba303ba028a55dd7
shuai.lin <shuai.lin@corp.to8to.com>
2025-01-08 12:48:14 +0800

    feat: 新增配置文件和 demo1

---
+++ tools/demo1.vue
@@ -0,0 +1,32 @@
+<script setup>
+import { ref } from 'vue';
+// 支持工具库 @linxs/toolkit
+import { clipboard } from "@linxs/toolkit";
+
+const msg = ref('hello');
+const tips = ref('')
+const handleCopy = () => {
+    clipboard({
+        data: msg.value,
+        success: (msg) => {
+            tips.value = msg;
+        },
+        error: (msg) => {
+            tips.value = msg;
+        },
+    });
+}
+</script>
+
+<template>
+    <div class="demo1">{{ msg }}</div>
+    <input v-model="msg">
+    <button type="button" @click="handleCopy">复制</button>
+    <div>{{ tips }}</div>
+</template>
+
+<style scoped>
+.demo1 {
+    color: red;
+}
+</style>
